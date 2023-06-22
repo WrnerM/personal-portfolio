a2643fcbcb1625e8db7be83adb32191fb2581f0e
Your Name <wrnermontala@gmail.com>
2023-06-22 23:55:13 +0800

    initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <>
+      <header className="font-sans font-medium text-xl p-5 border-solid border-black border-2">
+        Werner.dev
+      </header>
+    </>
+  );
+};
+
+export default Header;
