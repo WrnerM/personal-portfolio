e17a2faae3e7b1e3a2bdadb5b71be66b1343b3bc
Your Name <wrnermontala@gmail.com>
2023-09-28 22:51:08 +0800

    align items in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,14 @@ import React from "react";
 const Header = () => {
   return (
     <>
-      <header className="font-sans font-medium text-xl p-5 border-solid border-black border-2">
-        Werner.dev
+      <header className="flex justify-between font-sans font-medium text-xl p-5 border-solid border-black border-2">
+        <h1 className="mx-6">Werner.dev</h1>
+        <ul className="flex">
+          <li className="mx-6">Home</li>
+          <li className="mx-6">About</li>
+          <li className="mx-6">Projects</li>
+          <li className="mx-6">Contact</li>
+        </ul>
       </header>
     </>
   );
