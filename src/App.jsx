c3af52117c9962a68bed298434fe215d3017836e
Your Name <wrnermontala@gmail.com>
2023-11-10 23:54:58 +0800

    added projects component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Header from "./components/Header";
 import Introduction from "./components/Introduction";
 import About from "./components/About";
+import Projects from "./components/Projects";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <Introduction />
       <About />
+      <Projects />
     </>
   );
 }
---
+++ src/components/Projects.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Projects = () => {
+  return (
+    <div>
+      Projects:
+      <p>Coming Soon!</p>
+    </div>
+  );
+};
+
+export default Projects;
