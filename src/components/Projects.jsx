6f152c047896ba8300bb966723e9312e1b942e6e
Your Name <wrnermontala@gmail.com>
2023-11-13 00:10:39 +0800

    added projects tab, no project at the moment

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const Projects = () => {
   return (
-    <section id="Projects" className="flex justify-center mt-10 p-28">
+    <section id="Projects" className="flex justify-center mt-6 p-20">
       <p className="font-semibold text-4xl">Projects:</p>
-      <p>Coming Soon!</p>
+      <img src="src/assets/imgs/coming soon.jpg" alt="" className="w-3/6" />
     </section>
   );
 };
