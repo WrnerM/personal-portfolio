94230d4ec1ec1252df9ffa8c7586cdddc05991f3
Your Name <wrnermontala@gmail.com>
2023-11-22 01:50:01 +0800

    will add links to the paragraph

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -11,15 +11,16 @@ const Contact = () => {
           Let's Connect!
         </h2>
         <span className="text-3xl animate-bounce col-span-2"> 👇 </span>
-        {/* <p className="w-2/3">
+        <p className="mx-40 col-span-3">
           I'm open to exciting opportunities and collaborations. Feel free to
-          get in touch with me via email at YourEmail@example.com. Connect with
-          me on LinkedIn to stay updated on my professional journey: Your
-          LinkedIn Profile. For a more in-depth look at my coding projects, you
-          can find me on GitHub: Your GitHub Profile. Whether you have a
-          specific project in mind, want to discuss potential opportunities, or
-          just want to chat about all things tech, I'd love to hear from you!
-        </p> */}
+          get in touch with me via email at wrnermontala@gmail.com. Connect with
+          me on LinkedIn to stay updated on my professional journey:
+          https://www.linkedin.com/in/werner-montala/. For a more in-depth look
+          at my coding projects, you can find me on GitHub:
+          https://github.com/WrnerM. Whether you have a specific project in
+          mind, want to discuss potential opportunities, or just want to chat
+          about all things tech, I'd love to hear from you!
+        </p>
       </section>
     </>
   );
