5e2e9efb5c8a01775d218f69e70945e2fb7c9e28
Your Name <wrnermontala@gmail.com>
2023-12-01 23:56:18 +0800

    added footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,9 @@ export const Footer = () => {
   return (
     <>
       <section className="bg-slate-950 text-white p-12">
-        <p className="">Footer</p>
+        <p className="pl-20">
+          © 2023 Werner. Copyright 2023. All rights reserved
+        </p>
       </section>
     </>
   );
