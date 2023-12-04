be11caba254953903ff2afef8d4ddc0ad9b2d1b3
Your Name <wrnermontala@gmail.com>
2023-12-04 20:26:11 +0800

    fixed bug on header

--- src/components/Introduction.jsx
+++ src/components/Introduction.jsx
@@ -12,10 +12,13 @@ const Introduction = () => {
           className="h-96 w-74 mx-8 border-2 border-solid rounded-lg border-black"
         />
         <div className="mx-12 p-8 items-center space-y-4">
-          <h2 className="font-semibold text-3xl font-mono text-sky-600 ">
-            Hi, my name is Werner!
+          <h2 className="font-semibold text-5xl font-mono text-sky-600 ">
+            Hi, my name is <br />
+            Werner!
           </h2>
-          <p>An enthusiastic developer based in Manila, Philippines</p>
+          <p className="text-l">
+            A passionate developer based in Philippines 📍
+          </p>
           <div className="space-y-10">
             <span className="flex justify-center">
               <a
@@ -34,31 +37,32 @@ const Introduction = () => {
                 />
               </a>
             </span>
-            <p className="">Tech Stack</p>
+            <span className="font-mono">Tech Stack</span>
+            <span>⬇️</span>
             <span className="flex justify-between">
               <img
                 title="HTML"
-                className="h-16 w-16 transition-transform transform hover:-translate-y-3"
+                className="h-16 w-16 transition-transform hover:-translate-y-3"
                 src="src/assets/imgs/html.png"
               ></img>
               <img
                 title="CSS"
-                className="h-16 w-16 transition-transform transform hover:-translate-y-3"
+                className="h-16 w-16 transition-transform hover:-translate-y-3"
                 src="src/assets/imgs/css.png"
               ></img>
               <img
                 title="JavaScript"
-                className="h-16 w-16 transition-transform transform hover:-translate-y-3"
+                className="h-16 w-16 transition-transform hover:-translate-y-3"
                 src="src/assets/imgs/javascript.png"
               ></img>
               <img
                 title="React"
-                className="h-16 w-16 transition-transform transform hover:-translate-y-3"
+                className="h-16 w-16 transition-transform hover:-translate-y-3"
                 src="src/assets/imgs/react.png"
               ></img>
               <img
                 title="Tailwind"
-                className="h-16 w-16 transition-transform transform hover:-translate-y-3"
+                className="h-16 w-16 transition-transform hover:-translate-y-3"
                 src="src/assets/imgs/tailwind.jpg"
               ></img>
             </span>
